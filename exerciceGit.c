e93213c937ddc56f47f105d98c4c6ae94dc1949e
ismail mnifil <prenom.nom@cnrs.fr>
2021-01-18 10:46:37 +0100

    la premiere commit

---
+++ exerciceGit.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main ()
+{
+	char nom[3][100];
+	printf("bonjour les amis \n");
+	
+	
+	for (int i = 0 ; i < 3 ; i++)
+	{
+		printf("tapez votre nom ici :  ");
+		scanf("%s",nom[i]);	
+	}
+	
+		for (int i = 0 ; i < 3 ; i++)
+	{
+		printf("\nsalut : %s\n",nom[i]);	
+	}
+}
