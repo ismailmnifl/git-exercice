c1e16dd38e600dba1cf63351de96c5f72ab9201d
amine <saissihassaniamine@gmail.com>
2021-01-18 12:18:13 +0100

    new feature

--- exerciceGit.c
+++ exerciceGit.c
@@ -3,22 +3,26 @@
 
 int main ()
 {
-	char nom[3][100];
+
+	int fNum;
+
+	do
+	{
+		printf("how many friends do you have?");
+		scanf("%i", fNum);
+	} while(fNum <= 0);
+	char nom[fNum][100];
 	printf("bonjour les amis \n");
 	
 	
-	for (int i = 0 ; i < 3 ; i++)
+	for (int i = 0 ; i < fNum ; i++)
 	{
 		printf("tapez votre nom ici :  ");
 		scanf("%s",nom[i]);	
 	}
 	
-		for (int i = 0 ; i < 3 ; i++)
+		for (int i = 0 ; i < fNum ; i++)
 	{
 		printf("\nsalut : %s\n",nom[i]);	
-	}
-	while(1)
-	{
-		
 	}
 }
