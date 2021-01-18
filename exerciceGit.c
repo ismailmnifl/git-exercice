4107f16c80d3a7c96e9a8745f70f82eca3316ae3
ismail mnifil <prenom.nom@cnrs.fr>
2021-01-18 11:59:55 +0100

    salut

--- exerciceGit.c
+++ exerciceGit.c
@@ -3,26 +3,22 @@
 
 int main ()
 {
-	int fNum;
-
-	do
-	{
-		printf("How many friends do you have?");
-		scanf("%i", fNum);
-	}
-	while(fNum <= 0);
-
 	char nom[3][100];
 	printf("bonjour les amis \n");
 	
-	for (int i = 0 ; i < fNum ; i++)
+	
+	for (int i = 0 ; i < 3 ; i++)
 	{
 		printf("tapez votre nom ici :  ");
 		scanf("%s",nom[i]);	
 	}
 	
-		for (int i = 0 ; i < fNum ; i++)
+		for (int i = 0 ; i < 3 ; i++)
 	{
 		printf("\nsalut : %s\n",nom[i]);	
+	}
+	while(1)
+	{
+		
 	}
 }
